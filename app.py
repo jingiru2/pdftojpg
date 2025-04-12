fa5a540f392f1801463d9b6ff312a7e9339a8449
AIDAUM33\USER <skyjjw37@gmail.com>
2025-04-12 16:57:23 +0900

    포트 변경

--- app.py
+++ app.py
@@ -60,4 +60,4 @@ def convert_pdf():
         return jsonify({'success': False, 'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
